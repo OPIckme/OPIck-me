75f063aa22da1ee16e909ed0283b1e91c2228aaa
susan1231 <susan1231@naver.com>
2022-07-25 17:26:20 +0900

    Feat: Consult Entity, Repository 작성
    consult entity, repository 작성
    
    Fixes: # 163, # 164

---
+++ backend-java/src/main/java/com/ssafy/db/repository/ConsultRepository.java
@@ -0,0 +1,14 @@
+package com.ssafy.db.repository;
+
+import com.ssafy.db.entity.Consult;
+import com.ssafy.db.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ConsultRepository extends JpaRepository<Consult, Long> {
+
+    Optional<Consult> findByConsultId(Long consultId);
+}
