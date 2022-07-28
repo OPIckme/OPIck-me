2273efd6db3f24f576ff053c5ad2f20e0ce97fa5
ssomm-k <kww0915@gmail.com>
2022-07-28 13:11:29 +0900

    fix: simpleUI

--- frontend/src/components/LoginNavbar.vue
+++ frontend/src/components/LoginNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar" style="background-color:#0742F2;">
   <div class="container-fluid">
-    <a href="/" class="navbar-brand">
+    <a href="/mainpage" class="navbar-brand">
       <img src="../assets/logo.png" alt="" style="width:150px">
     </a>
     <a class="nav-link" @click.prevent="logOut">
@@ -11,25 +11,12 @@
   </div>
 </nav>
 </template>
-
 <script>
 export default {
   computed: {
     currentUser() {
       return this.$store.state.auth.user;
     },
-    showAdminBoard() {
-      if (this.currentUser && this.currentUser['roles']) {
-        return this.currentUser['roles'].includes('ROLE_ADMIN');
-      }
-      return false;
-    },
-    showModeratorBoard() {
-      if (this.currentUser && this.currentUser['roles']) {
-        return this.currentUser['roles'].includes('ROLE_MODERATOR');
-      }
-      return false;
-    }
   },
   methods: {
     logOut() {
