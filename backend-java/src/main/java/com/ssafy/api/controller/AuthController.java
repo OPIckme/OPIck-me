b0d6d027e34729660ed6029e112c084d8197e31c
na0173 <na0173@naver.com>
2022-08-02 15:12:54 +0900

    Refactor: 유저 로그인 api response 수정

--- backend-java/src/main/java/com/ssafy/api/controller/AuthController.java
+++ backend-java/src/main/java/com/ssafy/api/controller/AuthController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ssafy.api.request.UserLoginPostReq;
 import com.ssafy.api.response.UserLoginPostRes;
@@ -53,9 +50,9 @@ public class AuthController {
 		// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
 		if(passwordEncoder.matches(password, user.getPassword())) {
 			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
-			return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(userId)));
+			return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(userId),user));
 		}
 		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
-		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
+		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null,null));
 	}
 }
--- backend-java/src/main/java/com/ssafy/api/response/UserLoginPostRes.java
+++ backend-java/src/main/java/com/ssafy/api/response/UserLoginPostRes.java
@@ -2,6 +2,7 @@ package com.ssafy.api.response;
 
 import com.ssafy.common.model.response.BaseResponseBody;
 
+import com.ssafy.db.entity.User;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -17,12 +18,23 @@ public class UserLoginPostRes extends BaseResponseBody{
 	@ApiModelProperty(name="JWT 인증 토큰", example="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN...")
 	String accessToken;
 
+	Long id;
+	String username;
+	String email;
+	String nickname;
+	String role;
 	// 생성자랑 다른게 뭐냐
-	public static UserLoginPostRes of(Integer statusCode, String message, String accessToken) {
+	public static UserLoginPostRes of(Integer statusCode, String message, String accessToken, User user) {
 		UserLoginPostRes res = new UserLoginPostRes();
 		res.setStatusCode(statusCode);
 		res.setMessage(message);
 		res.setAccessToken(accessToken);
+
+		res.setId(user.getId());
+		res.setUsername(user.getUsername());
+		res.setEmail(user.getEmail());
+		res.setNickname(user.getNickname());
+		res.setRole(user.getRole());
 		return res;
 	}
 }
