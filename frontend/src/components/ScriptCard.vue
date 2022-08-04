db33cab09af036428ac0f517e1c861f065ef4820
ssomm-k <kww0915@gmail.com>
2022-08-04 15:01:19 +0900

    Feat:consultant API 연결

--- frontend/src/components/ScriptCard.vue
+++ frontend/src/components/ScriptCard.vue
@@ -16,9 +16,6 @@
 
 <script>
 import ScriptDeleteModal from './Modal/ScriptDeleteModal.vue';
-import axios from 'axios';
-const API_URL = 'http://localhost:8080/api/v1/script';
-
 export default {
     name: "ScriptCard",
     data() {
