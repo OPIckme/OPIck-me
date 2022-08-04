f6b76c8942aae9b55c48948740321cb1e76d71bd
leeda1231 <dlekdls1231@gmail.com>
2022-08-04 13:54:16 +0900

    Feat: delete api 연결
    script, feedback delete api 연결
    
    Resolves: #337

--- frontend/src/components/Modal/ScriptDeleteModal.vue
+++ frontend/src/components/Modal/ScriptDeleteModal.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Modal -->
-<div class="modal fade" id="delete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="false" style="background-color: rgba(0, 0, 0, 0.5);">
+<div class="modal fade" :id="modalId" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="false" style="background-color: rgba(0, 0, 0, 0.5);">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -10,9 +10,40 @@
         Script를 삭제하시겠습니까?
       </div>
       <div class="modal-footer">
-        <button data-bs-dismiss="modal" aria-label="Close" type="button" class="btn btn-primary">네</button>
+        <button @click="deleteScript" data-bs-dismiss="modal" aria-label="Close" type="button" class="btn btn-primary">네</button>
       </div>
     </div>
   </div>
 </div>
 </template>
+
+<script>
+import axios from 'axios';
+const API_URL = 'http://localhost:8080/api/v1/script';
+
+export default {
+    name: "ScriptDeleteModal",
+    data() {
+      return {
+        modalId:"delete"+this.scriptId
+      }
+    },
+    props : {
+      scriptId : String,
+    },
+    setup() {
+    },
+    methods: {
+    deleteScript(){
+      console.log(this.scriptId)
+      axios.delete(API_URL + `/${this.scriptId}`)
+    }
+    },
+}
+</script>
+
+<style scoped>
+p {
+  color : white;
+}
+</style>
