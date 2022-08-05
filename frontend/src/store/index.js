a452fa7c85c2c2b30b430223580a5c9a0c29694c
ssomm-k <kww0915@gmail.com>
2022-08-05 13:46:33 +0900

    Fix: 상담 상태 변경 바로 보임

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,13 @@ import { createStore } from "vuex";
 import { auth } from "./auth.module";
 import { script } from "./script";
 import { feedback } from "./feedback";
+import { consult } from "./consult";
 const store = createStore({
   modules: {
     auth,
     script,
     feedback,
+    consult
   },
 });
 export default store;
