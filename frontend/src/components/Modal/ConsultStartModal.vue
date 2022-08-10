650db01b0566c0f3c4c73f4bc4d31882108b8db6
이다인 <susan1231@naver.com>
2022-08-10 11:01:46 +0900

    Update ConsultStartModal.vue

--- frontend/src/components/Modal/ConsultStartModal.vue
+++ frontend/src/components/Modal/ConsultStartModal.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Modal -->
-<div class="modal" id="Consultstart" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="false" style="background-color: rgba(0, 0, 0, 0.5);" >
+<div class="modal" :id="consultStartModalId" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="false" style="background-color: rgba(0, 0, 0, 0.5);" >
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -8,7 +8,9 @@
       </div>
       <div class="modal-body">
         <p>상담을 시작합니다.</p>
-        <button type="button" class="btn btn-primary"  @click="startConsult()">Yes</button>
+        <div class="spinner-border text-primary" role="status">
+        <button type="button" class="btn btn-primary"  @click="[feedBack(), changeState()]">Yes</button>
+        </div>
       </div>
     </div>
   </div>
@@ -16,24 +18,30 @@
 </template>
 
 <script>
-import axios from 'axios';
-import { mapActions } from 'vuex';
 import {API_URL} from '@/api/http.js';
+import axios from 'axios';
 
 export default {
-  name: "ConsultStartModal",
-  props: {
+  data() {
+    return {
+      consultStartModalId : 'consult' + this.waitingconsult.id
+    }
+  },
+  props : {
     waitingconsult : Object
   },
   methods : {
-    ...mapActions(['fetchWaitingConsultMap']),
-    startConsult() {
+    feedBack() {
+            console.log(this.waitingconsult)
+            this.$router.push({name : "webrtcstudent", params: {room: this.waitingconsult.room}});
+        },
+    changeState(){
           axios.put(API_URL+ `/consult/complete/${this.waitingconsult.id}`
           ).then(res => {
-          this.fetchWaitingConsultMap()
+            console.log(res)
+            this.fetchWaitingConsultMap()
           })
-          this.$router.push("/webrtcstudent");
-        },
+      },
   }
 }
 </script>
