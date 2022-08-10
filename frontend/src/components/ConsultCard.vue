17b5b59a81505ce8005b78e5cc3ae1b35015e752
이다인 <susan1231@naver.com>
2022-08-10 11:00:09 +0900

    Update ConsultCard.vue

--- frontend/src/components/ConsultCard.vue
+++ frontend/src/components/ConsultCard.vue
@@ -3,7 +3,7 @@
   <div class="row">
     <p class="col">{{ waitingconsult.script.question.topic }}</p>
     <p class="col-6">{{ waitingconsult.script.question.questionContent }}</p>
-    <button class="btn col" style="color:white; background-color:#F2CB05;" data-bs-toggle="modal" data-bs-target="#Consultstart">상담하기</button>
+    <button @click="changeState" class="btn col" style="color:white; background-color:#F2CB05;" data-bs-toggle="modal" :data-bs-target="waitingconsultId">상담하기</button>
   </div>
 </div>
 <ConsultStartModal :waitingconsult="waitingconsult"></ConsultStartModal>
@@ -11,11 +11,23 @@
 
 <script>
 import ConsultStartModal from './Modal/ConsultStartModal.vue'
+import { mapActions } from 'vuex';
+
 
 export default {
     name: "ConsultCard",
+    data() {
+      return {
+        waitingconsultId : '#consult' + this.waitingconsult.id
+      }
+    },
     props : {
       waitingconsult : Object
+    },
+    setup() {
+    },
+    methods: {
+      ...mapActions(['fetchWaitingConsultMap']),
     },
     components : { ConsultStartModal }
 }
