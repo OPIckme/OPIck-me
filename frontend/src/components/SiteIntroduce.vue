9931b10fdb5a6207d138593e4396cf8d144fd451
ssomm-k <kww0915@gmail.com>
2022-08-12 11:15:55 +0900

    Fix:SiteIntroduce css 미완성

--- frontend/src/components/SiteIntroduce.vue
+++ frontend/src/components/SiteIntroduce.vue
@@ -1,77 +1,203 @@
 <template>
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner" >
-    <div class="carousel-item active">
-      <img src="../assets/1.png" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="../assets/2.png" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="../assets/3.png" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+<div class="text-center" style="background-color:white;">
+<h1>OPIck me</h1>
+  <h1 class="ml11">
+    <span class="text-wrapper">
+      <span class="line line1"></span>
+      <span class="letters">Of the OPIc</span>
+    </span>
+  </h1>
+  <h1 class="ml12">
+    <span class="text-wrapper2">
+      <span class="line line2"></span>
+      <span class="letters2">By the OPIc</span>
+    </span>
+  </h1>
+  <h1 class="ml13">
+    <span class="text-wrapper3">
+      <span class="line line3"></span>
+      <span class="letters3">For the OPIc</span>
+    </span>
+  </h1>
 </div>
-<div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-</div>
-
 </template>
 <script>
 export default {
-  name: "Home",
+  name: "SiteIntroduce",
+  mounted(){
+    var textWrapper = document.querySelector('.ml11 .letters');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+    var textWrapper2 = document.querySelector('.ml12 .letters2');
+    textWrapper2.innerHTML = textWrapper2.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter2'>$&</span>");
+    var textWrapper3 = document.querySelector('.ml13 .letters3');
+    textWrapper3.innerHTML = textWrapper3.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter3'>$&</span>");
+
+    anime.timeline({loop: true})
+      .add({
+        targets: '.ml11 .line',
+        scaleY: [0,1],
+        opacity: [0.5,1],
+        easing: "easeOutExpo",
+      }).add({
+        targets: '.ml11 .line',
+        translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+        easing: "easeOutExpo",
+      }).add({
+        targets: '.ml11 .letter',
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        offset: '-=775',
+        delay: (el, i) => 34 * (i+1)
+      }).add({
+        targets: '.ml11',
+        opacity: 0,
+        easing: "easeOutExpo",
+      }).add({
+        targets: '.ml12 .line',
+        scaleY: [0,1],
+        opacity: [0.5,1],
+        easing: "easeOutExpo",
+      }).add({
+        targets: '.ml12 .line',
+        translateX: [0, document.querySelector('.ml12 .letters2').getBoundingClientRect().width + 10],
+        easing: "easeOutExpo",
+      }).add({
+        targets: '.ml12 .letter2',
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        offset: '-=775',
+        delay: (el, i) => 34 * (i+1)
+      }).add({
+        targets: '.ml12',
+        opacity: 0,
+        easing: "easeOutExpo",
+      }).add({
+        targets: '.ml13 .line',
+        scaleY: [0,1],
+        opacity: [0.5,1],
+        easing: "easeOutExpo",
+      }).add({
+        targets: '.ml13 .line',
+        translateX: [0, document.querySelector('.ml13 .letters3').getBoundingClientRect().width + 10],
+        easing: "easeOutExpo",
+      }).add({
+        targets: '.ml13 .letter3',
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        offset: '-=775',
+        delay: (el, i) => 34 * (i+1)
+      }).add({
+        targets: '.ml13',
+        opacity: 0,
+        easing: "easeOutExpo",
+      }).add({
+        targets: '.ml11 .ml12 .ml13 .letter .letter2 .letter3',
+        scaleY: [0,1],
+        opacity: [0.5,1],
+        easing: "easeOutExpo",
+      });
+  }
 
 };
 </script>
 
-<style lang="scss" scoped>
- 
+<style scoped>
+.ml11 {
+  font-weight: 700;
+  font-size: 3.5em;
+}
+
+.ml11 .text-wrapper {
+  position: relative;
+  display: inline-block;
+  padding-top: 0.1em;
+  padding-right: 0.05em;
+  padding-bottom: 0.15em;
+}
+
+.ml11 .line {
+  opacity: 0;
+  position: absolute;
+  left: 0;
+  height: 100%;
+  width: 3px;
+  background-color: #fff;
+  transform-origin: 0 50%;
+}
+
+.ml11 .line1 { 
+  top: 0; 
+  left: 0;
+}
+
+.ml11 .letter {
+  display: inline-block;
+  line-height: 1em;
+}
+
+.ml12 {
+  font-weight: 700;
+  font-size: 3.5em;
+}
+
+.ml12 .text-wrapper2 {
+  position: relative;
+  display: inline-block;
+  padding-top: 0.1em;
+  padding-right: 0.05em;
+  padding-bottom: 0.15em;
+}
+
+.ml12 .line {
+  opacity: 0;
+  position: absolute;
+  left: 0;
+  height: 100%;
+  width: 3px;
+  background-color: #fff;
+  transform-origin: 0 50%;
+}
+
+.ml12 .line2 { 
+  top: 0; 
+  left: 0;
+}
+
+.ml12 .letter2 {
+  display: inline-block;
+  line-height: 1em;
+}
+
+.ml13 {
+  font-weight: 700;
+  font-size: 3.5em;
+}
+
+.ml13 .text-wrapper3 {
+  position: relative;
+  display: inline-block;
+  padding-top: 0.1em;
+  padding-right: 0.05em;
+  padding-bottom: 0.15em;
+}
+
+.ml13 .line {
+  opacity: 0;
+  position: absolute;
+  left: 0;
+  height: 100%;
+  width: 3px;
+  background-color: #fff;
+  transform-origin: 0 50%;
+}
+
+.ml13 .line3 { 
+  top: 0; 
+  left: 0;
+}
+
+.ml13 .letter3 {
+  display: inline-block;
+  line-height: 1em;
+}
 </style>
