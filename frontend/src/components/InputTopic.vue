84429f8441b3ad7a046b12b2e00564de6699c7d9
안태환 <atj14@naver.com>
2022-08-14 15:13:19 +0900

    Survey 선택 CSS

--- frontend/src/components/InputTopic.vue
+++ frontend/src/components/InputTopic.vue
@@ -1,7 +1,16 @@
 <template>
-    <div class="form-check form-check-inline">
+    <!-- 기존 v-for <div class="form-check form-check-inline">
         <input :id="item" @input="selectTopic" v-model="topic" class="form-check-input" type="radio" name="inlineRadioOptions" :value="item">
         <label class="form-check-label" :for="item">{{item}}</label>
+    </div> -->
+    
+    <div class="mb-3">
+        <div class="form_toggle row-vh d-flex flex-row justify-content-between">
+            <div class="form_radio_btn">
+                <input :id="item" @input="selectTopic" v-model="topic" class="form-check-input" type="radio" name="inlineRadioOptions" :value="item">
+                <label class="form-check-label" :for="item">{{item}}</label>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -28,7 +37,39 @@ export default {
 }
 </script>
 
-
 <style scoped>
+.form_radio_btn {
+    width: 47%;
+    height : 45px;
+    border: 1px solid #EAE7E7;
+    border-radius: 10px;
+}
 
+.form_radio_btn input[type=radio] {
+    display: none;
+}
+.form_radio_btn label {
+    display: block;
+    border-radius: 10px;
+    margin: 0 auto;
+    text-align: center;
+    height: -webkit-fill-available;
+    line-height: 45px;
+}
+/* Checked */
+.form_radio_btn input[type=radio]:checked + label {
+    background: #184DA0;
+    color: #fff;
+}
+    
+/* Hover */
+.form_radio_btn label:hover {
+    color: #666;
+}
+    
+/* Disabled */
+.form_radio_btn input[type=radio] + label {
+    background: #F9FAFC;
+    color: #666;
+}
 </style>
